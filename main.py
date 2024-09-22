5acc1e4768a5cb5a157ade9798ae488534086109
yokamoto5742-h <yokamoto5742@gmail.com>
2024-09-22 13:43:09 +0900

    VERSION = "1.0.2" LAST_UPDATED = "2024/09/22"

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHB
 from PyQt5.QtWidgets import QStyleFactory
 import socket
 
-VERSION = "1.0.1"
+VERSION = "1.0.2"
 LAST_UPDATED = "2024/09/22"
 
 
