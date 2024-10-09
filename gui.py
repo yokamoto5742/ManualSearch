cc35819a437eed73eb27389696521124df1dea5f
yokamoto5742-h <yokamoto5742@gmail.com>
2024-10-09 19:30:00 +0900

    コード分割の一旦停止

--- gui.py
+++ gui.py
@@ -1,12 +1,15 @@
+import os
+
 from PyQt5.QtWidgets import (QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton,
-                             QTextEdit, QListWidget, QComboBox, QCheckBox, QFileDialog, QMessageBox)
+                             QTextEdit, QListWidget, QComboBox, QCheckBox, QFileDialog, QMessageBox, QInputDialog)
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 
 from config_manager import ConfigManager
 from auto_close_message import AutoCloseMessage
 from search_manager import SearchManager
-from file_opener import FileOpener
+from file_opener import FileOpener, open_folder
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -61,8 +64,88 @@ class MainWindow(QMainWindow):
         self.layout.addWidget(self.search_type_combo)
 
     def setup_directory_selection(self):
-        # ディレクトリ選択のUI設定（省略）
-        pass
+        dir_layout = QVBoxLayout()
+
+        # コンボボックスの設定
+        self.dir_combo = QComboBox()
+        directories = self.config_manager.directories
+        self.dir_combo.addItems(directories)
+        self.dir_combo.setEditable(True)
+
+        last_directory = self.config_manager.last_directory
+        if last_directory and last_directory in directories:
+            self.dir_combo.setCurrentText(last_directory)
+        elif directories:
+            self.dir_combo.setCurrentText(directories[0])
+
+        self.dir_combo.currentTextChanged.connect(self.update_last_directory)
+        self.dir_combo.editTextChanged.connect(self.validate_directory)
+        dir_layout.addWidget(self.dir_combo)
+
+        # ボタンとチェックボックスの設定
+        dir_buttons_layout = QHBoxLayout()
+        dir_add_button = QPushButton("追加")
+        dir_add_button.clicked.connect(self.add_directory)
+        dir_edit_button = QPushButton("編集")
+        dir_edit_button.clicked.connect(self.edit_directory)
+        dir_delete_button = QPushButton("削除")
+        dir_delete_button.clicked.connect(self.delete_directory)
+        self.include_subdirs_checkbox = QCheckBox("サブフォルダを含む")
+        self.include_subdirs_checkbox.setChecked(True)
+
+        dir_buttons_layout.addWidget(dir_add_button)
+        dir_buttons_layout.addWidget(dir_edit_button)
+        dir_buttons_layout.addWidget(dir_delete_button)
+        dir_buttons_layout.addWidget(self.include_subdirs_checkbox)
+        dir_buttons_layout.addStretch(1)  # 右側に余白を追加
+
+        dir_layout.addLayout(dir_buttons_layout)
+        self.layout.addLayout(dir_layout)
+
+    def add_directory(self):
+        directory = QFileDialog.getExistingDirectory(self, "フォルダを選択")
+        if directory:
+            current_dirs = self.config_manager.directories
+            if directory not in current_dirs:
+                current_dirs.append(directory)
+                self.config_manager.directories = current_dirs
+                self.dir_combo.addItem(directory)
+            self.dir_combo.setCurrentText(directory)
+            self.config_manager.last_directory = directory
+
+    def update_last_directory(self, directory):
+        if os.path.isdir(directory):
+            self.config_manager.last_directory = directory
+
+    def validate_directory(self, directory):
+        if not os.path.isdir(directory):
+            self.dir_combo.setStyleSheet("background-color: #FFCCCC;")
+        else:
+            self.dir_combo.setStyleSheet("")
+
+    def edit_directory(self):
+        current_dir = self.dir_combo.currentText()
+        if current_dir:
+            new_dir, ok = QInputDialog.getText(self, "フォルダ編集", "新しいフォルダパス:", QLineEdit.Normal,
+                                               current_dir)
+            if ok and new_dir:
+                current_dirs = self.config_manager.directories
+                index = current_dirs.index(current_dir)
+                current_dirs[index] = new_dir
+                self.config_manager.directories = current_dirs
+                self.dir_combo.setItemText(self.dir_combo.currentIndex(), new_dir)
+
+    def delete_directory(self):
+        current_dir = self.dir_combo.currentText()
+        if current_dir:
+            reply = QMessageBox.question(self, '確認',
+                                         f"本当に「{current_dir}」を削除しますか？",
+                                         QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+            if reply == QMessageBox.Yes:
+                current_dirs = self.config_manager.directories
+                current_dirs.remove(current_dir)
+                self.config_manager.directories = current_dirs
+                self.dir_combo.removeItem(self.dir_combo.currentIndex())
 
     def setup_results_list(self):
         self.results_list = QListWidget()
