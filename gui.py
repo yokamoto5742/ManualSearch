43784ae60dd602b27e2532d2780246b2573266fa
yokamoto5742-h <yokamoto5742@gmail.com>
2024-10-12 14:54:43 +0900

    gui.pyの修正

--- gui.py
+++ gui.py
@@ -1,40 +1,15 @@
 import os
 import re
-from PyQt5.QtCore import Qt, QTimer
+import subprocess
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont, QColor
 from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton, \
     QTextEdit, QFileDialog, QListWidget, QListWidgetItem, QMessageBox, QCheckBox, QComboBox, QInputDialog, \
-    QProgressDialog, QLabel, QStyleFactory, QApplication
+    QProgressDialog, QStyleFactory, QApplication
 from file_searcher import FileSearcher
 from pdf_handler import open_pdf, wait_for_acrobat, navigate_to_page, highlight_pdf
 from text_handler import open_text_file
-import subprocess
-
-
-class AutoCloseMessage(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent, Qt.Window | Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
-        self.setStyleSheet("""
-            background-color: #f0f0f0;
-            border: 1px solid #cccccc;
-            border-radius: 5px;
-        """)
-        self.layout = QVBoxLayout(self)
-        self.label = QLabel(self)
-        self.label.setAlignment(Qt.AlignCenter)
-        self.layout.addWidget(self.label)
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.close)
-
-    def show_message(self, message, duration=1000):
-        self.label.setText(message)
-        self.adjustSize()
-        parent = self.parent()
-        if parent:
-            geometry = parent.geometry()
-            self.move(geometry.center() - self.rect().center())
-        self.show()
-        self.timer.start(duration)
+from auto_close_message import AutoCloseMessage
 
 
 class MainWindow(QMainWindow):
