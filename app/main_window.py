1d23827d3906d97ac6dc9360b24db674285a4e8b
yokamoto5742-h <yokamoto5742@gmail.com>
2025-07-17 18:16:15 +0900

    ✨ refactor(app/main_window.py): 更新タイトルからバージョン表記を削除しシンプル化

--- app/main_window.py
+++ app/main_window.py
@@ -6,9 +6,9 @@ from PyQt5.QtWidgets import (
     QStyleFactory, QApplication
 )
 
-from widgets.auto_close_message_widget import AutoCloseMessage
 from service.file_opener import FileOpener
 from utils.config_manager import ConfigManager
+from widgets.auto_close_message_widget import AutoCloseMessage
 from widgets.directory_widget import DirectoryWidget
 from widgets.results_widget import ResultsWidget
 from widgets.search_widget import SearchWidget
@@ -20,7 +20,7 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.config_manager = config_manager
 
-        self.setWindowTitle(f"マニュアル検索アプリ v{VERSION}")
+        self.setWindowTitle(f"マニュアル検索 v{VERSION}")
         QApplication.setStyle(QStyleFactory.create('Fusion'))
 
         self._setup_window_geometry()
