9a7c17a2b218b2c1869006b803f0c05a0d19a259
yokamoto5742-h <yokamoto5742@gmail.com>
2026-06-04 13:25:13 +0900

    ✨ feat(version): アプリケーションのバージョンを1.4.0に更新

--- build.py
+++ build.py
@@ -1,10 +1,7 @@
 import subprocess
 
-from scripts.version_manager import update_version
-
 
 def build_executable():
-    new_version = update_version()
     subprocess.run([
         "pyinstaller",
         "--name=マニュアル検索",
@@ -15,7 +12,7 @@ def build_executable():
         "main.py"
     ])
 
-    print(f"Executable built successfully. Version: {new_version}")
+    print(f"Executable built successfully.")
     return new_version
 
 
